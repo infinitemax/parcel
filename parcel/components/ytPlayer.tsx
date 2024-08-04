a06e615752d22b2fb77bea54328be7bcb1d62a99
Max BB <max.bb@revitt.consulting>
2024-08-04 16:33:24 +0100

    (url) input: add core functionality

--- parcel/components/ytPlayer.tsx
+++ parcel/components/ytPlayer.tsx
@@ -16,6 +16,7 @@ const YtPlayer = () => {
       // https://developers.google.com/youtube/player_parameters
       // autoplay: 1,
       controls: 0,
+      rel: 0,
     },
   };
 
@@ -31,23 +32,44 @@ const YtPlayer = () => {
     }
   };
 
-  const [videoId, setVideoId] = useState<string>("");
+  const [videoId, setVideoId] = useState<string>("Q_ReXuz3xww");
   const [videoUrl, setVideoUrl] = useState<string>("");
 
   const getVideoId = (url: string) => {
+    const idArray = url.split("v=");
 
-    const idArray = url.split("v=")
+    const initialId = idArray[idArray.length - 1];
 
-    const initialId = idArray[idArray.length - 1]
+    const secondIdArray = initialId.split("&t=");
 
-    const secondIdArray = initialId.split("&t=")
-
-    setVideoId(secondIdArray[0])
+    setVideoId(secondIdArray[0]);
   };
 
-  useEffect(() => {
-    console.log(videoId)
-  }, [videoId])
+  // set video play time
+
+  const [minMusicTime, setMinMusicTime] = useState<number>(0);
+
+  const [maxMusicTime, setMaxMusicTime] = useState<number>(0);
+
+  const randomIntFromInput = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const printRandom = () => {
+    console.log(randomIntFromInput(minMusicTime, maxMusicTime));
+  };
+
+  const passTheParcel = () => {
+    const randomTime = randomIntFromInput(minMusicTime, maxMusicTime);
+
+    console.log("duration will be: ", randomTime);
+
+    const durationInMilliseconds = randomTime * 1000;
+    play();
+    setTimeout(() => {
+      pause();
+    }, durationInMilliseconds);
+  };
 
   return (
     <div className="bg-slate-500 p-10 flex flex-col justify-center">
@@ -58,7 +80,14 @@ const YtPlayer = () => {
           placeholder="Enter YouTube link..."
           onChange={(e) => setVideoUrl(e.target.value)}
         ></input>
-        <button className="rounded-md bg-green-200 px-4 h-10" onClick={() => {getVideoId(videoUrl)}}>GO!</button>
+        <button
+          className="rounded-md bg-green-200 px-4 h-10"
+          onClick={() => {
+            getVideoId(videoUrl);
+          }}
+        >
+          GO!
+        </button>
       </div>
 
       <div className={`p-4 bg-white w-${opts.width} flex justify-center`}>
@@ -85,6 +114,45 @@ const YtPlayer = () => {
           }}
         >
           Play
+        </button>
+        <button
+          className="px-4 py-2 bg-slate-300 m-2"
+          onClick={(e) => {
+            passTheParcel();
+          }}
+        >
+          Pass the Parcel!
+        </button>
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="time-input">
+          Minimum music:{" "}
+          <input
+            id="time-input"
+            type="number"
+            value={minMusicTime}
+            onChange={(e) => {
+              setMinMusicTime(parseInt(e.target.value));
+            }}
+          ></input>
+        </label>
+        <label htmlFor="time-input">
+          Maximum music:{" "}
+          <input
+            id="time-input"
+            type="number"
+            value={maxMusicTime}
+            onChange={(e) => {
+              setMaxMusicTime(parseInt(e.target.value));
+            }}
+          ></input>
+        </label>
+        <button
+          onClick={() => {
+            printRandom();
+          }}
+        >
+          Print random
         </button>
       </div>
     </div>
